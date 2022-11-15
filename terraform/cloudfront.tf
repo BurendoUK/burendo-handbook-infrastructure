e99e35886d0702114c81d76ec69a4b4c12443376
ChrisScottThomas <chris.scott-thomas@burendo.com>
2022-11-15 08:17:10 +0000

    Has to include forwarded_values
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -16,6 +16,14 @@ resource "aws_cloudfront_distribution" "handbook_distribution" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
 
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 0
@@ -28,6 +36,14 @@ resource "aws_cloudfront_distribution" "handbook_distribution" {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
 
     min_ttl                = 0
     default_ttl            = 0
