a94881c8f876ab292ad86fa0e438e11dace93ff6
ChrisScottThomas <chris.scott-thomas@burendo.com>
2022-11-15 07:29:34 +0000

    Remove caching from CF
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -16,18 +16,10 @@ resource "aws_cloudfront_distribution" "handbook_distribution" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
 
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 0
+    max_ttl                = 0
   }
 
   # Cache behavior with precedence 0
@@ -37,18 +29,9 @@ resource "aws_cloudfront_distribution" "handbook_distribution" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
 
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
     min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
+    default_ttl            = 0
+    max_ttl                = 0
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
   }
