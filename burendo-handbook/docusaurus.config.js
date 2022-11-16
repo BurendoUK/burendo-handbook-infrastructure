772c21145e4229465b80b949d7c339352e8cbb3c
ChrisScottThomas <chris.scott-thomas@burendo.com>
2022-11-16 08:54:53 +0000

    Add search plugin
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- burendo-handbook/docusaurus.config.js
+++ burendo-handbook/docusaurus.config.js
@@ -13,6 +13,7 @@ const config = {
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/burendo_outline.png",
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
