34f0d0915e6c646d320045c2fdf5b0d42c8e3562
ChrisScottThomas <chris.scott-thomas@burendo.com>
2023-02-15 07:32:29 +0000

    optimised code and unit tests
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- lambda/__tests__/index.test.js
+++ lambda/__tests__/index.test.js
@@ -7,3 +7,119 @@
 // Test that the token is verified successfully if all conditions are met.
 // For each test case, you will need to create a mock implementation of the necessary dependencies(e.g.request, jwt) 
 // that returns the expected results for the given input parameters.Additionally, you will need to create mock events that simulate the AWS Lambda function input.
+
+// Unit tests for Cognito authentication Lambda function
+
+'use strict';
+const jwt = require('jsonwebtoken');
+const jwkToPem = require('jwk-to-pem');
+const request = require('request-promise');
+
+const { handler } = require('./index');
+
+jest.mock('request-promise');
+
+describe('Cognito authentication Lambda function', () => {
+  const USERPOOLID = 'test-pool-id';
+  const region = 'eu-west-2';
+  const iss = `https://cognito-idp.${region}.amazonaws.com/${USERPOOLID}`;
+  const jwksUrl = `https://cognito-idp.${region}.amazonaws.com/${USERPOOLID}/.well-known/jwks.json`;
+
+  const pems = {
+    'test-kid': 'mock-pem'
+  };
+
+  const mockHeaders = {
+    'Authorization': 'Bearer mock-token'
+  };
+
+  const mockEvent = {
+    Records: [{
+      cf: {
+        request: {
+          headers: mockHeaders
+        }
+      }
+    }]
+  };
+
+  const mockToken = jwt.sign({ token_use: 'access', iss: iss }, 'mock-secret', { header: { kid: 'test-kid' } });
+
+  const response401 = { status: '401', statusDescription: 'Unauthorized', body: 'Invalid access token' };
+  const response200 = { status: '200', statusDescription: 'OK' };
+
+  const spyJWKSRequest = jest.spyOn(request, 'get').mockImplementation(() => Promise.resolve({ keys: [{ kid: 'test-kid' }] }));
+
+  beforeAll(async () => {
+    request.get.mockImplementation(() => Promise.resolve({
+      keys: [
+        {
+          kid: 'test-kid',
+          alg: 'RS256',
+          use: 'sig',
+          n: 'mock-n-value',
+          e: 'AQAB',
+          x5c: ['mock-x5c-value']
+        }
+      ]
+    }));
+
+    await getJWKS();
+  });
+
+  afterEach(() => {
+    spyJWKSRequest.mockClear();
+  });
+
+  afterAll(() => {
+    spyJWKSRequest.mockRestore();
+  });
+
+  it('should return 401 if no authorization header found', async () => {
+    const result = await handler({ Records: [{ cf: { request: { headers: {} } } }] });
+    expect(result).toEqual(response401);
+  });
+
+  it('should return 401 if the token is not JWT', async () => {
+    const result = await handler({ Records: [{ cf: { request: { headers: { 'Authorization': 'invalid-token' } } } }] });
+    expect(result).toEqual(response401);
+  });
+
+  it('should return 401 if token is not from the User Pool', async () => {
+    const mockTokenWithInvalidIssuer = jwt.sign({ iss: 'https://invalid-issuer.com' }, 'mock-secret');
+    const result = await handler({ Records: [{ cf: { request: { headers: { 'Authorization': `Bearer ${ mockTokenWithInvalidIssuer }` } } } }] });
+    expect(result).toEqual(response401);
+  });
+
+  it('should return 401 if the token is not an access token', async () => {
+    const mockTokenWithInvalidUse = jwt.sign({ token_use: 'id' }, 'mock-secret', { header: { kid: 'test-kid' } });
+    const result = await handler({ Records: [{ cf: { request: { headers: { 'Authorization': `Bearer ${ mockTokenWithInvalidUse }` } } } }] });
+    expect(result).toEqual(response401);
+  });
+
+  it('should return 401 if the token is invalid', async () => {
+    const result = await handler(mockEvent);
+    expect(result).toEqual(response401);
+  });
+
+  it('should return 401 if the jwks request fails', async () => {
+    spyJWKSRequest.mockImplementationOnce(() => Promise.reject(new Error('Failed to retrieve JWKS')));
+    const result = await handler(mockEvent);
+    expect(result).toEqual(response401);
+  });
+
+  it('should return 401 if the kid is not found in pems', async () => {
+    const spyJWKSToPem = jest.spyOn(jwkToPem, 'jwkToPem').mockImplementation(() => 'mock-pem');
+    const mockTokenWithInvalidKid = jwt.sign({ token_use: 'access', iss: iss }, 'mock-secret', { header: { kid: 'invalid-kid' } });
+    const result = await handler({ Records: [{ cf: { request: { headers: { 'Authorization': `Bearer ${ mockTokenWithInvalidKid }` } } } }] });
+    expect(result).toEqual(response401);
+    spyJWKSToPem.mockRestore();
+  });
+
+  it('should return 200 if the token is valid', async () => {
+    const mockDecodedJwt = { header: { kid: 'test-kid' }, payload: { sub: 'test-user-id', iss: iss, token_use: 'access' } };
+    const mockValidToken = jwt.sign(mockDecodedJwt.payload, 'mock-pem', { header: mockDecodedJwt.header });
+    const result = await handler({ Records: [{ cf: { request: { headers: { 'Authorization': `Bearer ${ mockValidToken }` } } } }] });
+    expect(result).toEqual(response200);
+  });
+})
--- lambda/index.js
+++ lambda/index.js
@@ -1,118 +1,106 @@
 'use strict';
 const jwt = require('jsonwebtoken');
 const jwkToPem = require('jwk-to-pem');
-const request = require('request');
+const request = require('request-promise');
 
-let USERPOOLID = process.env.USERPOOLID
+const USERPOOLID = process.env.USERPOOLID;
+const region = 'eu-west-2';
+const iss = `https://cognito-idp.${region}.amazonaws.com/${USERPOOLID}`;
+const jwksUrl = `https://cognito-idp.${region}.amazonaws.com/${USERPOOLID}/.well-known/jwks.json`;
 
-let jwksUrl = 'https://cognito-idp.' + region + '.amazonaws.com/' + USERPOOLID + '/.well-known/jwks.json';
-let options = { json: true };
-var region = 'eu-west-2';
-var iss = 'https://cognito-idp.' + region + '.amazonaws.com/' + USERPOOLID;
-var pems;
+let pems = {};
 
-pems = {};
-request(jwksUrl, options, (error, res, body) => {
-  if (error) {
-    return console.log(error)
-  };
-  if (!error && res.statusCode == 200) {
-    if (!error && response.statusCode === 200) {
-      const keys = body['keys'];
-      const jwk = keys.find(k => k.kid === token.header.kid);
-      if (!jwk) {
-        callback('Invalid token');
-      } else {
-        const pem = jwkToPem(jwk);
-        jwt.verify(token, pem, function (err, decoded) {
-          if (err) {
-            callback('Invalid token');
-          } else {
-            // Check if the user belongs to the correct Cognito User Pool
-            if (decoded.iss === 'https://cognito-idp.${region}.amazonaws.com/${USERPOOLID}') {
-              // Allow the user to access the resource
-              callback(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
-            } else {
-              callback('Unauthorized');
-            }
-          }
-        });
-      }
-    } else {
-      callback('Unable to retrieve jwks');
-    }
-  };
-});
+async function getJWKS() {
+  const options = { json: true, uri: jwksUrl };
+  const body = await request(options);
+  console.log('JWKS response:', body);
+  const keys = body['keys'];
+  console.log('JWKS keys:', keys);
+  for (let key of keys) {
+    pems[key.kid] = jwkToPem(key);
+  }
+}
+
+getJWKS();
 
 const response401 = {
   status: '401',
   statusDescription: 'Unauthorized'
 };
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context) => {
   const cfrequest = event.Records[0].cf.request;
   const headers = cfrequest.headers;
+
   console.log('getting started');
-  console.log('USERPOOLID=' + USERPOOLID);
-  console.log('region=' + region);
-  console.log('pems=' + pems);
+  console.log(`USERPOOLID=${USERPOOLID}`);
+  console.log(`region=${region}`);
+  console.log(`pems=${pems}`);
 
   //Fail if no authorization header found
   if (!headers.authorization) {
     console.log("no auth header");
-    callback(null, response401);
-    return false;
+    return {
+      ...response401,
+      body: 'No authorization header found',
+    };
   }
 
   //strip out "Bearer " to extract JWT token only
-  var jwtToken = headers.authorization[0].value.slice(7);
-  console.log('jwtToken=' + jwtToken);
+  const jwtToken = headers.authorization[0].value.slice(7);
+  console.log(`jwtToken=${jwtToken}`);
 
   //Fail if the token is not jwt
-  var decodedJwt = jwt.decode(jwtToken, { complete: true });
+  const decodedJwt = jwt.decode(jwtToken, { complete: true });
   if (!decodedJwt) {
     console.log("Not a valid JWT token");
-    callback(null, response401);
-    return false;
+    return {
+      ...response401,
+      body: 'Invalid JWT token',
+    };
   }
 
   //Fail if token is not from your UserPool
-  if (decodedJwt.payload.iss != iss) {
+  if (decodedJwt.payload.iss !== iss) {
     console.log("invalid issuer");
-    callback(null, response401);
-    return false;
+    return {
+      ...response401,
+      body: 'Invalid token issuer',
+    };
   }
 
   //Reject the jwt if it's not an 'Access Token'
-  if (decodedJwt.payload.token_use != 'access') {
+  if (decodedJwt.payload.token_use !== 'access') {
     console.log("Not an access token");
-    callback(null, response401);
-    return false;
+    return {
+      ...response401,
+      body: 'Invalid token type',
+    };
   }
 
   //Get the kid from the token and retrieve corresponding PEM
-  var kid = decodedJwt.header.kid;
-  var pem = pems[kid];
+  const kid = decodedJwt.header.kid;
+  const pem = pems[kid];
   if (!pem) {
     console.log('Invalid access token');
-    callback(null, response401);
-    return false;
+    return {
+      ...response401,
+      body: 'Invalid access token',
+    };
   }
 
   //Verify the signature of the JWT token to ensure it's really coming from your User Pool
-  jwt.verify(jwtToken, pem, { issuer: iss }, function (err, payload) {
-    if (err) {
-      console.log('Token failed verification');
-      callback(null, response401);
-      return false;
-    } else {
-      //Valid token. 
-      console.log('Successful verification');
-      //remove authorization header
-      delete cfrequest.headers.authorization;
-      //CloudFront can proceed to fetch the content from origin
-      callback(null, cfrequest);
-      return true;
-    }
-  });
-};
+  try {
+    const payload = jwt.verify(jwtToken, pem, { issuer: iss });
+    console.log("validated JWT");
+
+    //Valid token. Process the request further
+
+  } catch (e) {
+    console.log("Invalid access token");
+    return {
+      ...response401,
+      body: 'Invalid access token',
+    };
+  }
+}
