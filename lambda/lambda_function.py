d7fd9766eb865449b85a402984fd8f2fc1ea70a1
ChrisScottThomas <chris.scott-thomas@burendo.com>
2023-02-20 11:33:14 +0000

    Create Python AWS lambda with requirements file and instructions
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,6 +1,11 @@
 import json
-import urllib.request
+import requests
 import jwt
+import os
+
+user_pool_id = os.environ["USER_POOL_ID"]
+app_client_id = os.environ["APP_CLIENT_ID"]
+region = os.environ["AWS_DEFAULT_REGION"]
 
 def lambda_handler(event, context):
     # Check if the Authorization header is present in the request
@@ -13,10 +18,11 @@ def lambda_handler(event, context):
     # Extract the access token from the Authorization header
     token = event["headers"]["Authorization"].split()[1]
     
+    
     # Retrieve the JSON Web Key Set (JWKS) for the user pool to extract the public key corresponding to the "kid" header in the access token
-    jwks_url = "https://cognito-idp.{region}.amazonaws.com/{user_pool_id}/.well-known/jwks.json".format(region="YOUR_AWS_REGION", user_pool_id="YOUR_USER_POOL_ID")
-    response = urllib.request.urlopen(jwks_url)
-    jwks = json.loads(response.read())
+    jwks_url = "https://cognito-idp.{region}.amazonaws.com/{user_pool_id}/.well-known/jwks.json".format(region="region", user_pool_id="user_pool_id")
+    response = requests.get(jwks_url)
+    jwks = json.loads(response.text)
     
     # Verify the signature of the access token using the public key
     unverified_header = jwt.get_unverified_header(token)
@@ -43,8 +49,8 @@ def lambda_handler(event, context):
             token,
             rsa_key,
             algorithms=["RS256"],
-            audience="YOUR_APP_CLIENT_ID",
-            issuer="https://cognito-idp.{region}.amazonaws.com/{user_pool_id}".format(region="YOUR_AWS_REGION", user_pool_id="YOUR_USER_POOL_ID"),
+            audience="app_client_id",
+            issuer="https://cognito-idp.{region}.amazonaws.com/{user_pool_id}".format(region="region", user_pool_id="user_pool_id"),
             options={"verify_exp": False}
         )
         if payload["token_use"] != "access":
