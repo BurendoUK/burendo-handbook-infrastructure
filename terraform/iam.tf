086618afb730420429ab2fcf2997fc7a8a68be47
ChrisScottThomas <chris.scott-thomas@burendo.com>
2023-02-22 16:07:22 +0000

    Allow Lambda@Edge to invoke decode Lambda
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,8 +3,11 @@ data "aws_iam_policy_document" "lambda_assume_role_policy" {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com",
+        "edgelambda.amazonaws.com"
+      ]
     }
   }
 }
@@ -12,4 +15,22 @@ data "aws_iam_policy_document" "lambda_assume_role_policy" {
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy" "iam_for_lambda" {
+  name   = "iam_for_lambda"
+  role   = aws_iam_role.iam_for_lambda.id
+  policy = data.aws_iam_policy_document.iam_for_lambda.json
+}
+
+data "aws_iam_policy_document" "iam_for_lambda" {
+  statement {
+    actions = [
+      "lambda:InvokeFunction",
+    ]
+
+    resources = [
+      "arn:aws:lambda:us-east-1:${local.account[local.environment]}:function:${aws_lambda_function.decode_lambda.function_name}",
+    ]
+  }
 }
