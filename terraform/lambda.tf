d2ddbdbbdf9a66754bccf575cf68026c81347af1
ChrisScottThomas <chris.scott-thomas@burendo.com>
2023-02-24 08:53:16 +0000

    Remove old test and lambda
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,22 +1,3 @@
-resource "aws_lambda_function" "test_lambda" {
-  provider         = aws.northvirginia
-  filename         = "../lambda/lambda.zip"
-  function_name    = "handbook-auth"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "index.handler"
-  publish          = true
-  source_code_hash = filebase64sha256("../lambda/lambda.zip")
-
-  runtime = "python3.8"
-
-  environment {
-    variables = {
-      USER_POOL_ID  = aws_cognito_user_pool.burendo.id
-      APP_CLIENT_ID = aws_cognito_user_pool_client.burendo_client.id
-    }
-  }
-}
-
 resource "aws_lambda_function" "auth_lambda" {
   provider      = aws.northvirginia
   filename      = "../lambda/auth.zip"
