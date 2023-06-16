1ac01712fbd58a8e89ad5dba76f64c47d8973c04
Steven Burton <steven.burton@burendo.com>
2023-06-16 16:55:01 +0100

    Add alarm for health check for handbook and send the alarms to SNS for future slack integration

---
+++ terraform/cloudwatch.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudwatch_metric_alarm" "burendo_handbook_health_check_alarm" {
+  provider                  = "aws"
+  alarm_name                = "${local.environment}-burendo-handbook-health-check-alarm"
+  comparison_operator       = "LessThanThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "HealthCheckStatus"
+  namespace                 = "AWS/Route53"
+  period                    = "60"
+  statistic                 = "Minimum"
+  threshold                 = "1"
+  insufficient_data_actions = []
+  alarm_actions             = ["${aws_sns_topic.topic.arn}"]
+  alarm_description         = "Send an alarm if burendo handbook is down in ${local.environment}"
+
+  dimensions {
+    HealthCheckId = "${aws_route53_health_check.burendo_handbook_health_check_alarm_topic.id}"
+  }
+}
