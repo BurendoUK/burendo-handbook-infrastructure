42b3eb0907ef6bf5588f5d51e3a07e9665ac24f6
Steven Burton <steven.burton@burendo.com>
2023-06-16 17:01:18 +0100

    Small fixes

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_metric_alarm" "burendo_handbook_health_check_alarm" {
-  provider                  = "aws"
+  provider                  = aws
   alarm_name                = "${local.environment}-burendo-handbook-health-check-alarm"
   comparison_operator       = "LessThanThreshold"
   evaluation_periods        = "1"
@@ -9,10 +9,10 @@ resource "aws_cloudwatch_metric_alarm" "burendo_handbook_health_check_alarm" {
   statistic                 = "Minimum"
   threshold                 = "1"
   insufficient_data_actions = []
-  alarm_actions             = ["${aws_sns_topic.topic.arn}"]
+  alarm_actions             = ["${aws_sns_topic.burendo_handbook_health_check_alarm_topic.arn}"]
   alarm_description         = "Send an alarm if burendo handbook is down in ${local.environment}"
 
-  dimensions {
-    HealthCheckId = "${aws_route53_health_check.burendo_handbook_health_check_alarm_topic.id}"
+  dimensions = {
+    HealthCheckId = "${aws_route53_health_check.burendo_handbook_health_check.id}"
   }
 }
