a4a7a753c18f428403e173770b4a0b6ec71b745a
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-03-05 10:51:54 +0000

    create user
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/db.tf
+++ terraform/db.tf
@@ -1,10 +1,27 @@
-resource "mongodbatlas_cluster" "cluster-test" {
+resource "mongodbatlas_cluster" "handbook-db" {
   project_id = "67c806f87ceec5230502cfc1"
-  name       = "burendo-handbook-cluster"
+  name       = "burendo-handbook-db"
 
   # Provider Settings "block"
   provider_name               = "TENANT"
   backing_provider_name       = "AWS"
-  provider_region_name        = "EU_WEST_2"
+  provider_region_name        = "EU_WEST_1"
   provider_instance_size_name = "M0"
+}
+
+resource "mongodbatlas_database_user" "handbook-user" {
+  username           = local.mongodb_user
+  password           = local.mongodb_password
+  project_id         = "67c806f87ceec5230502cfc1"
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "readAnyDatabase"
+    database_name = "admin"
+  }
+
+  scopes {
+    name = mongodbatlas_cluster.handbook-db.name
+    type = "CLUSTER"
+  }
 }
