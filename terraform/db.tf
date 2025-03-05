6bb5a6fa73d9423330a5971f0581aa5a957d6fbd
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-03-05 10:12:57 +0000

    updated information to create DB
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/db.tf
+++ terraform/db.tf
@@ -1,5 +1,5 @@
 resource "mongodbatlas_cluster" "cluster-test" {
-  project_id = "burendo-handbook-cluster"
+  project_id = "67c806f87ceec5230502cfc1"
   name       = "burendo-handbook-cluster"
 
   # Provider Settings "block"
