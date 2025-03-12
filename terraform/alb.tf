998adbc0acb7ecad1e90e3c9f7785f105f076211
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-03-12 11:36:46 +0000

    Add listeners for 3000 & 80
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -7,20 +7,58 @@ resource "aws_lb" "burendo_handbook_internal_alb" {
   depends_on         = [aws_internet_gateway.burendo_handbook_igw]
 }
 
-resource "aws_lb_target_group" "burendo_handbook_tg" {
-  name     = "burendo-handbook-target-group"
+resource "aws_lb_target_group" "burendo_handbook_native_tg" {
+  name     = "burendo-handbook-native-tg"
   port     = 3000 # Change from 80 to 3000
   protocol = "HTTP"
   vpc_id   = aws_vpc.burendo_handbook_vpc.id
 }
 
-resource "aws_lb_listener" "burendo_handbook_listener" {
+resource "aws_lb_listener" "burendo_handbook_native_listener" {
   load_balancer_arn = aws_lb.burendo_handbook_internal_alb.arn
   port              = 3000
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.burendo_handbook_tg.arn
+    target_group_arn = aws_lb_target_group.burendo_handbook_native_tg.arn
   }
 }
+
+resource "aws_lb_target_group" "burendo_handbook_http_tg" {
+  name     = "burendo-handbook-http-tg"
+  port     = 80 # Change from 80 to 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.burendo_handbook_vpc.id
+}
+
+resource "aws_lb_listener" "burendo_handbook_http_listener" {
+  load_balancer_arn = aws_lb.burendo_handbook_internal_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.burendo_handbook_http_tg.arn
+  }
+}
+
+# resource "aws_lb_target_group" "burendo_handbook_https_tg" {
+#   name     = "burendo-handbook-https-tg"
+#   port     = 443 # Change from 80 to 3000
+#   protocol = "HTTPS"
+#   vpc_id   = aws_vpc.burendo_handbook_vpc.id
+# }
+
+# resource "aws_lb_listener" "burendo_handbook_https_listener" {
+#   load_balancer_arn = aws_lb.burendo_handbook_internal_alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = aws_acm_certificate.burendo_handbook.arn
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.burendo_handbook_https_tg.arn
+#   }
+# }
